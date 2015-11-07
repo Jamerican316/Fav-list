4af455e803cfb7164403047936211d4518b4f103
Jamerican316 <karanja.broomfield@gmail.com>
2015-11-06 22:51:18 -0800

    changed html and css to make app more simple

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	
-})
+	$("#list_items").on("keyup change", function(){
+		var value = $("value").val();
+		$(".list > div").append('<div class="item">' + value 
+			+ '</div>');
+	});
+});
+
+

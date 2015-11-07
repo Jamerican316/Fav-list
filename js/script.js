cac6630d09a67aeda1e1cc075ebb2d8c5b66711b
JS Bangs <jaspax@gmail.com>
2015-11-07 00:41:09 -0600

    got basic code working to append list items

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+    $("form").on("submit", function(evt) {
+        var list_items = $("#list_items");
+        var value = list_items.val();
+        $("#list").append("<div class='item'>" + value + "</div>");
+
+        list_items.val("");
+
+        evt.preventDefault();
+    });
 })

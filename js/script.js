745c3d5e9b2161bd35934f510db6e53d178967c0
Jamerican316 <karanja.broomfield@gmail.com>
2015-11-09 16:12:03 -0800

    Element append to shopping list delete/strikethrough work-in-progress

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
-    var i = 1;
+
     $("form").on("submit", function(evt) {
-       //create a loop to ++i each item
         var list_items = $("#list_items");
         var value = list_items.val();
-        var div = $('<div>').addClass('item').text(value);
-        var div2 = $('<div>').addClass('tally');
-        var count = $('<p>').text(i);
-        var icon = $('<i>').addClass('fa fa-info');
-        var div3 = $('<div>').addClass('info');
-        $(div3).append(icon);
-        $(div2).append(count);
-        $(div).append(div2, div3)
-        $("#items").prepend(div);
-
+        var div = $('<div>').addClass('item')
+        //leaving code in case I decide to add item qty later
+        //var div2 = $('<div>').addClass('tally');
+        //var itemQnty = $('<input type="text" name="" id="item_Qty" placeholder="1"/>');
+        var p = $('<p>').text(value);
+        var div2 = $('<div>').attr('id', 'list_item_edit_delete');
+        var icon = $('<span>').addClass('fa fa-info');
+        $(div2).append(icon);
+        $(div).append(div2)
+        $(div).prepend(p)
+        $("#items").append(div);
         list_items.val("");
         evt.preventDefault();
-    
+    })
+    $('#list_item_edit_delete').on('p', 'click', function(){
+        $(this).toggleClass('strike');
     });
-   
 });

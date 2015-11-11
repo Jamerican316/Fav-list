2c7c7ffc8165a9d64e85ce99770ab39719ef2109
Jamerican316 <karanja.broomfield@gmail.com>
2015-11-10 17:34:43 -0800

    everything works except the delete function

--- js/script.js
+++ js/script.js
@@ -4,9 +4,6 @@ $(document).ready(function() {
         var list_items = $("#list_items");
         var value = list_items.val();
         var div = $('<div>').addClass('item')
-        //leaving code in case I decide to add item qty later
-        //var div2 = $('<div>').addClass('tally');
-        //var itemQnty = $('<input type="text" name="" id="item_Qty" placeholder="1"/>');
         var p = $('<p>').text(value);
         var div2 = $('<div>').attr('id', 'list_item_edit_delete');
         var icon = $('<span>').addClass('fa fa-info');
@@ -17,7 +14,12 @@ $(document).ready(function() {
         list_items.val("");
         evt.preventDefault();
     })
-    $('#list_item_edit_delete').on('p', 'click', function(){
-        $(this).toggleClass('strike');
+    $('div').on('click', 'p', function(){
+        $(this).closest('p').toggleClass('strike');
     });
 });
+
+
+//leaving code in case I decide to add item qty later
+        //var div2 = $('<div>').addClass('tally');
+        //var itemQnty = $('<input type="text" name="" id="item_Qty" placeholder="1"/>');
